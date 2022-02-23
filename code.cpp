7f77400180274fe7febb6f62fbd8692acb75ffee
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 21:19:19 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,23 @@
+// Solution of 1012 - Area with Bonus in beecrowd
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+  double A, B, C, pi = 3.14159, TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
+  cin >> A >> B >> C;
+  TRIANGULO = (1 / 2.0) * (A * C);
+  CIRCULO = pi * (C * C);
+  TRAPEZIO = ((A + B) / 2) * C;
+  QUADRADO = B * B;
+  RETANGULO = A * B;
+  cout << "TRIANGULO: " << fixed << setprecision (3) << TRIANGULO << endl;
+  cout << "CIRCULO: " << fixed << setprecision (3) << CIRCULO << endl;
+  cout << "TRAPEZIO: " << fixed << setprecision (3) << TRAPEZIO << endl;
+  cout << "QUADRADO: " << fixed << setprecision (3) << QUADRADO << endl;
+  cout << "RETANGULO: " << fixed << setprecision (3) << RETANGULO << endl;
+    system("pause");
+    return 0;
+}
